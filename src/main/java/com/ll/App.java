e1ebb1ace1f1bb29b8299dbc4f9ed5f0857ecfab
Dumbbell04 <Dumbbelloper04@gmail.com>
2024-02-23 20:44:41 +0900

    1단계 완료

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -3,13 +3,17 @@ package com.ll;
 import java.util.Scanner;
 
 class App {
-    Scanner in  = new Scanner(System.in);
-    void run() {
-        System.out.println("프로그램 실행");
-        System.out.println("== 명언 앱 ==");
-        System.out.print("명령) ");
-        String 명령어 = in.nextLine();
-        System.out.println("입력받은 명령어) " + 명령어);
+    Scanner in = new Scanner(System.in);
 
+    void run() {
+        System.out.println("== 명언 앱 ==");
+        while (true) {
+            System.out.print("명령) ");
+            String 명령어 = in.nextLine();
+            System.out.println("입력받은 명령어) " + 명령어);
+            if (명령어.equals("종료")) {
+                break;
+            }
+        }
     }
 }
