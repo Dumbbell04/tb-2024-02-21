4f69d1371a711b9ebba867c5d80c903cc95a7b4e
Dumbbell04 <Dumbbelloper04@gmail.com>
2024-02-26 21:40:27 +0900

    5단계 완료(2)

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,42 +1,57 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class App {
     Scanner in = new Scanner(System.in);
-    String[] 명언등록 = new String[100];
-    String[] 작가등록 = new String[100];
 
     void run() {
-        int wisenum = 0;
+
         System.out.println("== 명언 앱 ==");
+        int wisenum = 0;
+
+        List<Wisesaying> wisesayings = new ArrayList<>();
         while (true) {
             System.out.print("명령) ");
+
             String 명령어 = in.nextLine();
             System.out.println("입력받은 명령어) " + 명령어);
             if (명령어.equals("종료")) {
                 break;
             } else if (명령어.equals("등록")) {
-                wisenum++;
+
 
                 System.out.print("명언 : ");
-                명언등록[wisenum] = in.nextLine();
-                System.out.print("작가 : ");
-                작가등록[wisenum] = in.nextLine();
+                String 명언등록 = in.nextLine();
+                String content = 명언등록;
 
-                System.out.println("명언) " + 명언등록[wisenum]);
-                System.out.println("작가) " + 작가등록[wisenum]);
+                System.out.print("작가 : ");
+                String 작가등록 = in.nextLine();
+                String authorName = 작가등록;
+                System.out.println("명언) " + 명언등록);
+                System.out.println("작가) " + 작가등록);
+                wisenum++;
+                int id = wisenum;
 
                 System.out.println(wisenum + "번 명언이 등록되었습니다.");
-
+                Wisesaying wisesaying = new Wisesaying(id, content, authorName);
+                wisesayings.add(wisesaying);
             } else if (명령어.equals("목록")) {
                 System.out.println("번호 / 작가 / 명언 ");
                 System.out.println("----------------------");
 
-                for (int listnum = 1; listnum <= wisenum; listnum++) {
-                    System.out.println(listnum + " / " + 작가등록[listnum] + " / " + 명언등록[listnum]);
+                if (wisesayings.isEmpty()) {
+                    System.out.println("등록된 명언이 없습니다.");
+                }
+                for (int i = wisesayings.size() - 1; i >= 0; i--) {
+                    Wisesaying wisesaying = wisesayings.get(i);
+                    System.out.printf("%d / %s / %s\n", wisesaying.id, wisesaying.content, wisesaying.authorName);
                 }
             }
+
         }
     }
 }
+
---
+++ src/main/java/com/ll/Wisesaying.java
@@ -0,0 +1,13 @@
+package com.ll;
+
+public class Wisesaying {
+    int id;
+    String content;
+    String authorName;
+
+    public Wisesaying(int id, String content, String authorName) {
+        this.id = id;
+        this.content = content;
+        this.authorName = authorName;
+    }
+}
